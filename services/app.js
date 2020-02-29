1b391f45443ca6d9676ff88ff1d490cb7a14cc17
Sam Devaprasad <unclesam@umich.edu>
2020-02-28 20:51:05 -0500

    create a working API that retrieves data from db & commit db file

--- services/app.js
+++ services/app.js
@@ -1,10 +1,52 @@
-var express = require("express");
+// init project
+var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
-app.get("/names", (req, res, next) => {
-    res.json(["Sam Devaprasad","Not Sam","Mark Cinali","Not Mark"]);
-   });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.listen(6063, () => {
- console.log("Server running on port 6063");
+// init sqlite db
+var fs = require('fs');
+var dbFile = './walmart_inhome.db';
+var exists = fs.existsSync(dbFile);
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(dbFile);
+
+// check if db has data
+db.serialize(function(){
+  if (!exists) {
+    console.log('oh no nothing exists!');
+  }
+  else {
+    console.log('awesome we have data!');
+  }
 });
 
+// create endpoints
+app.get('/users', function(request, response) {
+  db.all('SELECT * from users', function(err, rows) {
+    response.send(JSON.stringify(rows));
+  });
+});
+
+app.get('/items', function(request, response) {
+    db.all('SELECT * from items', function(err, rows) {
+      response.send(JSON.stringify(rows));
+    });
+  });
+
+app.get('/orders', function(request, response) {
+db.all('SELECT * from orders', function(err, rows) {
+    response.send(JSON.stringify(rows));
+    });
+});
+
+app.get('/order_items', function(request, response) {
+db.all('SELECT * from order_items', function(err, rows) {
+    response.send(JSON.stringify(rows));
+    });
+});
+
+app.listen(6063, () => {
+    console.log("Server running on port 6063");
+   });
